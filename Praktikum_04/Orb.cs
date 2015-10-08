9ba95a975b2dc9c4ba06a02140ad841d6fde7109
Fabian Hediger <hedigerf@gmail.com>
2015-10-08 13:06:13 +0200

    init praktikum 4

---
+++ Praktikum_04/Orb.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Praktikum_04
+{
+    abstract class Orb
+    {
+        const double G = 30; //6.673e-11
+
+        protected Bitmap bitmap;
+        protected Vektor posNew, pos;
+        protected Vektor v0;
+        protected string name;
+        protected double masse;
+
+        public Vektor Pos
+        {
+            get { return pos; }
+        }
+
+        public Vektor Velocity
+        {
+            get { return v0; }
+        }
+
+        public double Mass
+        {
+            get { return masse; }
+        }
+        public abstract void Draw(Graphics g);
+
+        public void Move()
+        {
+            pos = posNew;
+        }
+
+        public Orb(string name, double x, double y, double vx, double vy, double m)
+        {
+            bitmap = new Bitmap(name + ".gif");
+            bitmap.MakeTransparent(bitmap.GetPixel(1, 1));
+            pos = new Vektor(x, y, 0);
+            v0 = new Vektor(vx, vy, 0);
+            masse = m;
+            this.name = name;
+        }
+
+        public virtual void CalcPosNew(IList<Orb> space)
+        {
+
+            // TODO
+            // berechne neue Beschleunigung
+            //XXXXXXXXXXXXXXXXXXXXXXX
+          //neue Position berechnen
+      double t = 3;
+           // posNew = pos + v0 * t + (t * t) * a;
+            //v0 = v0 + t * a;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+
+
+    }
+}
---
+++ Praktikum_04/Vektor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Praktikum_04
+{
+    public struct Vektor
+    {
+
+        public static Vektor operator +(Vektor a, Vektor b)
+        {
+            return new Vektor(a.XValue + b.XValue, a.YValue + b.YValue, a.ZValue + b.ZValue);
+        }
+
+        public static Vektor operator -(Vektor a, Vektor b)
+        {
+            return new Vektor(a.XValue - b.XValue, a.YValue - b.YValue, a.ZValue - b.ZValue);
+        }
+
+        public static Vektor operator *(Vektor a, Vektor b)
+        {
+            return new Vektor(a.YValue * b.ZValue - a.ZValue * b.YValue, a.ZValue * b.XValue - a.XValue * b.ZValue, a.XValue * b.YValue - a.YValue * b.XValue);
+        }
+        public static implicit operator Vektor(double a)
+        {
+            return new Vektor(a, 0, 0);
+        }
+
+        public static explicit operator double (Vektor a)
+        {
+            return Math.Pow((Math.Pow(a.XValue,2.0) + Math.Pow(a.YValue,2.0) + Math.Pow(a.ZValue,2.0)),0.5);
+        }
+
+        public double this[int x]
+        {
+            get
+            {
+                if (x == 0)
+                {
+                    return XValue;
+                }
+                else if (x == 1)
+                {
+                    return YValue;
+                }
+                else
+                {
+                    return ZValue;
+                }
+            }
+        }
+
+
+        public double XValue { get; }
+
+        public double YValue { get; }
+
+        public double ZValue { get; }
+
+        public Vektor(double xValue, double yValue, double zValue)
+        {
+            this.XValue = xValue;
+            this.YValue = yValue;
+            this.ZValue = zValue;
+        }
+
+        public override string ToString()
+        {
+            return "[" + XValue + "," + YValue + "," + ZValue + "]";
+        }
+        //public Vektor
+
+
+
+    }
+}
+
