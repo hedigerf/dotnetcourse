00b67b9bd25f81364d8fbbbda9eaba1bfe3cb4d6
Fabian Hediger <hedigerf@gmail.com>
2015-10-08 14:23:59 +0200

    ticker

--- Praktikum_04/Program.cs
+++ Praktikum_04/Program.cs
@@ -17,13 +17,23 @@ namespace Praktikum_04
         public Program()
         {
             this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = new System.Drawing.Bitmap("..\\..\\Starsbg.jpg");
             this.ClientSize = new System.Drawing.Size(1000, 1000);
+            Timer timer = new Timer();
+            timer.Enabled = true;
+            timer.Interval = 50;
+            timer.Tick += tick;
             Planet jupiter = new Planet("jupiter", 200, 200, 50, 50, 40);
             Planet mars = new Planet("mars", 500, 100, 0, 100, 20);
+            Spaceship enterpise = new Spaceship("enterprise",400,400,200,200,10);
             drawers += jupiter.Draw;
             drawers += mars.Draw;
-            //this.Paint += new Sy
+            drawers += enterpise.Draw;
+        }
 
+        private void tick(object sender, EventArgs e)
+        {
+            Refresh();
         }
 
         protected override void OnPaint(PaintEventArgs e)
