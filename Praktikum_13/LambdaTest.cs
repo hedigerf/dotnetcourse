492bd748883668d12d36a2454d191f606adc293a
Fabian Hediger <hedigerf@gmail.com>
2015-12-10 12:36:42 +0100

    Praktikum 13

---
+++ Praktikum_13/LambdaTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum13
+{
+    class LambdaTest
+    {
+        public delegate int Function(int x);
+
+        public delegate int Function4(int k1, int k2, int k3,int x);
+
+        public int[] ProcessData(Function f,int[] array)
+        {
+            int[] res = new int[array.Length];
+            int i = 0;
+            foreach(int value in array)
+            {
+                res[i++] = f(value);
+            }
+            return res;
+        }
+
+        public int[] ProcessData2(Function4 f, int x, int[] array)
+        {
+            int[] res = new int[array.Length / 3];
+            int i = 0;
+            var list = new List<int>();
+            foreach (int value in array)
+            {
+                list.Add(value);
+                if(list.Count == 3)
+                {
+                    int first = list[0];
+                    res[i++] = f(list[0],list[1],list[2], x);
+                    list.Clear();
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Praktikum_13/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = new int[] { 1, 2, 3, 4, 5, 6, 2, 1, 8 };            var lambdaTest = new LambdaTest();
+
+            //a)
+            Console.WriteLine("a)");            var res =lambdaTest.ProcessData(i => (int)Math.Pow(i, 2), values);            Array.ForEach(res, elem => Console.WriteLine(elem));
+
+            //b)
+            Console.WriteLine("b)");
+            var res2 = lambdaTest.ProcessData2((k1,k2,k3,x) => k1* (int)Math.Pow(x, 2)+k2* (int)Math.Pow(x, 1) + k3,2,values);
+            Array.ForEach(res2, elem => Console.WriteLine(elem));
+
+            //c)
+            Console.WriteLine("c)");
+            var res3 =values.Select(elem => elem % 3 == 0 ? elem : 0);
+            foreach(int elem in res3)
+            {
+                Console.WriteLine(elem);
+            }
+
+            //d)
+            Console.WriteLine("d)");
+            IEnumerable<int> scoreQuery =
+                from score in values
+                    where score > 3
+                        where score < 6
+                            select score;
+            foreach(int elem in scoreQuery)
+            {
+                Console.WriteLine(elem);
+            }
+
+
+            System.Threading.Thread.Sleep(4000);
+
+        }
+
+
+    }
+}
